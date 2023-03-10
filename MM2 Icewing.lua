32ec5626ef65947cc0fdb4757b56ef659fcad776
Micro0226 <127509385+Micro0226@users.noreply.github.com>
2023-03-10 22:19:31 +0300

    Add files via upload

---
+++ MM2 Icewing.lua
@@ -0,0 +1,6 @@
+--Murder Mystery 2 Icewing
+--THIS SCRIPT IS CLIENTSIDE
+workspace:WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("KnifeDisplay"):WaitForChild("Mesh").MeshId = "http://roblox.com/asset/?id=3183449780"
+workspace:WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("KnifeDisplay"):WaitForChild("Mesh").TextureId = "http://roblox.com/asset/?id=2279588369"
+workspace:WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("KnifeDisplay"):WaitForChild("Mesh").Scale = Vector3.new(0.085, 0.085, 0.085)
+workspace:WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("KnifeDisplay").Size = Vector3.new(0.4, 4.05, 1.8)
---
+++ MM2 Void.lua
@@ -0,0 +1,6 @@
+--Murder Mystery 2 Void
+--THIS SCRIPT IS CLIENTSIDE
+workspace:WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("KnifeDisplay"):WaitForChild("Mesh").MeshId = "http://roblox.com/asset/?id=6600901997"
+workspace:WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("KnifeDisplay"):WaitForChild("Mesh").TextureId = "http://roblox.com/asset/?id=11548074269"
+workspace:WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("KnifeDisplay"):WaitForChild("Mesh").Scale = Vector3.new(1, 1, 1)
+workspace:WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("KnifeDisplay").Size = Vector3.new(0.4, 3, 0.7)
